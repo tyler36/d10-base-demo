904179d40436e6e857db1a8f4fc5a2703b15fc0b
tyler36 <7234392+tyler36@users.noreply.github.com>
2023-12-21 01:29:31 +0000

    remove demo javascript

--- web/themes/custom/js/app.js
+++ web/themes/custom/js/app.js
@@ -8,26 +8,3 @@
 //   }
 
 // };
-(function (Drupal, once) {
-  Drupal.behaviors.exampleModule = {
-    attach: function (context, settings) {
-      once('header', 'html', context).forEach(
-        item => {
-          const messages = new Drupal.Message();
-          const messageId = messages.add('test message');
-          // messages.remove(messageId);
-          messages.add('test message', {type: 'warning'});
-          messages.add('test message', {type: 'error'});
-          // Clear ALL
-          // messages.clear();
-        }
-      )
-    },
-    detach: function (context, settings) {
-      once('header', 'html', context).forEach(
-        item => console.log('goodbye world!')
-      )
-      console.log('goodbye world! ... out')
-    }
-  };
-})(Drupal, once);
